88e3df4c9a898e4eef7297f39f67df6b66012919
Jack <babybishops@gmail.com>
2020-02-19 16:14:05 +0100

    almost

--- options/main.js
+++ options/main.js
@@ -1,38 +1,44 @@
 async function main()
 {
     const clientID = "988121232658-ib7a5b3eu5j5eq0ca7mbn6c3rld5m99l.apps.googleusercontent.com";
+    const clientSecret = "TmtljFJguO98j4si2LIQMR15";
     const redirectURL = "https://87dc662381fddaa0fac473c102b78da4b346adea.extensions.allizom.org/";
     const scope = "https://www.googleapis.com/auth/drive.metadata.readonly";
 
-    var url = "https://accounts.google.com/o/oauth2/auth?";
-    url += "response_type=token"
-    url += "&client_id=" + clientID;
-    url += "&redirect_uri=" + encodeURIComponent(redirectURL)
-    url += "&scope=" + encodeURIComponent(scope);
+    let authCode = null;
+    getAccessToken: {
+        let url = "https://accounts.google.com/o/oauth2/auth";
+        url += "?response_type=code"
+        url += "&access_type=offline";
+        url += "&client_id=" + clientID;
+        url += "&redirect_uri=" + encodeURIComponent(redirectURL)
+        url += "&scope=" + encodeURIComponent(scope);
+        let resultURL = await browser.identity.launchWebAuthFlow(
+            {
+                url: url,
+                interactive: true,
+            }
+        );
+        authCode = new URL(resultURL).searchParams.get("code");
+    }
 
-    var resultURL = await browser.identity.launchWebAuthFlow(
+    let response = await fetch(
         {
-            url: url,
-            interactive: true,
+            url: "https://oauth2.googleapis.com/token",
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(
+                {
+                    code: authCode,
+                    client_id: clientID,
+                    client_secret: clientSecret,
+                    redirect_uri: redirectURL,
+                    grant_type: "authorization_code"
+                }
+            )
         }
     );
-    let components = new URL(resultURL).hash.substring(1).split("&");
-    let accessToken = null;
-    for (let component of components)
-    {
-        let [name, value] = component.split("=");
-        if (name === "access_token")
-        {
-            accessToken = value;
-        }
-    }
-
-    let response = await fetch("https://www.googleapis.com/drive/v3/files?access_token=" + accessToken);
-    let json = await response.json();
-    for (let file of json.files)
-    {
-        console.log(file.name);
-    }
+    let body = await response.json();
 }
 
 main();
