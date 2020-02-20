3d0e9b1ebff92047e807d3d660ca5da6da08b73a
Jack <->
2020-02-20 18:35:37 +0100

    refactor

--- options/main.js
+++ options/main.js
@@ -1,13 +1,16 @@
 const clientID = "988121232658-ib7a5b3eu5j5eq0ca7mbn6c3rld5m99l.apps.googleusercontent.com";
 const clientSecret = "TmtljFJguO98j4si2LIQMR15";
 
-async function getRefreshToken()
+async function getAccessToken(storageKey)
 {
-    const redirectURL = "https://87dc662381fddaa0fac473c102b78da4b346adea.extensions.allizom.org/";
-    const scope = "https://www.googleapis.com/auth/drive.metadata.readonly";
+    var { refreshToken } = await browser.storage.local.get(storageKey);
+    var accessToken;
 
-    let authCode = null;
-    getAccessToken: {
+    if (!refreshToken)
+    {
+        const redirectURL = "https://87dc662381fddaa0fac473c102b78da4b346adea.extensions.allizom.org/";
+        const scope = "https://www.googleapis.com/auth/drive.metadata.readonly";
+
         let url = "https://accounts.google.com/o/oauth2/auth";
         url += "?response_type=code"
         url += "&access_type=offline";
@@ -20,47 +23,27 @@ async function getRefreshToken()
                 interactive: true,
             }
         );
-        authCode = new URL(resultURL).searchParams.get("code");
-    }
+        let authCode = new URL(resultURL).searchParams.get("code");
 
-    let response = await fetch(
-        "https://oauth2.googleapis.com/token",
-        {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(
-                {
-                    code: authCode,
-                    client_id: clientID,
-                    client_secret: clientSecret,
-                    redirect_uri: redirectURL,
-                    grant_type: "authorization_code"
-                }
-            )
-        }
-    );
-    let responseJSON = await response.json();
-    let accessToken = responseJSON["access_token"];
-    let refreshToken = responseJSON["refresh_token"];
-    
-    return { accessToken: accessToken, refreshToken: refreshToken };
-}
-
-async function main()
-{
-    var { refreshToken } = await browser.storage.local.get("refreshToken");
-    var accessToken;
-
-    if (!refreshToken)
-    {
-        var { accessToken, refreshToken } = await getRefreshToken();
-
-        if (refreshToken)
-        {
-            browser.storage.local.set(
-                { refreshToken: refreshToken }
-            );
-        }
+        let response = await fetch(
+            "https://oauth2.googleapis.com/token",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(
+                    {
+                        code: authCode,
+                        client_id: clientID,
+                        client_secret: clientSecret,
+                        redirect_uri: redirectURL,
+                        grant_type: "authorization_code"
+                    }
+                )
+            }
+        );
+        let responseJSON = await response.json();
+        accessToken = responseJSON["access_token"];
+        let refreshToken = responseJSON["refresh_token"];
     }
     else
     {
@@ -84,6 +67,12 @@ async function main()
         accessToken = responseJSON["access_token"];
     }
 
+    return accessToken;
+}
+
+async function main()
+{
+    let accessToken = await getAccessToken("refreshToken");
     let response = await fetch("https://www.googleapis.com/drive/v3/files?access_token=" + accessToken);
     let json = await response.json();
     for (let file of json.files)
